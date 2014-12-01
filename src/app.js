e5cd25d045ea7f9e863745e0035123112bb9996e
Sergei Liski <sergeiliski@gmail.com>
2014-12-01 19:50:34 +0200

    printtaaminen sivulle #päivitys
    Laitoin diveihin points ja time mihin aattelin et ne vois tulla.
    Koitin myös asetella itse kirjaimet keskelle niiden laatikkoja.
    Nyt toimii vaan et painaa F5 niin se tulosta venäjänkielisen ja sille
    vastineen. Eli ei paljoakaan vielä

--- src/app.js
+++ src/app.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
 						"П", "Р", "C", "T", "Y", "Ф", "X", "Ц", "Ч", "Ш", "Щ", "Ы", "Э", "Ю", "Я"];
 	var nonCyrilicArray = ["A", "B", "V", "G", "D", "YE", "YO", "ZH", "Z", "I", "J", "K", "L", "M", "N", "O",
 							"P", "R", "C", "T", "U", "F", "KH", "TS", "CH", "SH", "SHCH", "IH", "E", "YU", "YA"];
+
+
+	var random = Math.floor(Math.random() * cyrilicArray.length);
+	$("#cyrillicContent").text(cyrilicArray[random])
+	$("#nonCyrillicContent").text(nonCyrilicArray[random])
 });
